fb44958c3dba2b3dfe0be1b72764780749b5e4c0
Demian Bonzi <demianzx@live.com.ar>
2024-09-24 00:08:18 -0300

    Objetos

---
+++ Taller de Programacion/Objetos/Practica 2/Ejercicio4P2/src/ejercicio4p2/Ejercicio4P2.java
@@ -0,0 +1,28 @@
+/*
+4- Sobre un nuevo programa, modifique el ejercicio anterior para considerar que:
+a) Durante el proceso de inscripción se pida a cada persona sus datos (nombre, DNI, edad)
+y el día en que se quiere presentar al casting. La persona debe ser inscripta en ese día, en el
+siguiente turno disponible. En caso de no existir un turno en ese día, informe la situación.
+La inscripción finaliza al llegar una persona con nombre “ZZZ” o al cubrirse los 40 cupos de
+casting.
+Una vez finalizada la inscripción:
+b) Informar para cada día: la cantidad de inscriptos al casting ese día y el nombre de la
+persona a entrevistar en cada turno asignado.
+
+ */
+package ejercicio4p2;
+
+/**
+ *
+ * @author DEMIA
+ */
+public class Ejercicio4P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
